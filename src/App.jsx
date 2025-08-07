513d6e861473eb9a2d3e853376f0499ad8e21987
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-08-07 23:30:13 +0530

    Day-1 (assets, Navbar , Routing)

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MyAppointment from './pages/MyAppointment'
+import MyProfile from './pages/MyProfile'
+import Doctor from './pages/Doctor'
+import Appointments from './pages/Appointments'
+import NavBar from './components/NavBar'
+import Contact from './pages/Contact'
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+         <Route path='/doctor' element={<Doctor/>} />
+          <Route path='/doctor/:speciality' element={<Doctor/>} />
+          <Route path='/login' element={<Login/>} />
+           <Route path='/about' element={<About/>} />
+            <Route path='/my-appointment' element={<MyAppointment/>} />
+             <Route path='/my-profile' element={<MyProfile/>} />
+               <Route path='/appointment/:docId' element={<Appointments/>} />
+                 <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
