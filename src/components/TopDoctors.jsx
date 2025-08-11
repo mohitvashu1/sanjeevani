e06a387e224a31564db33656706894c497ee36d8
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-08-11 23:57:31 +0530

    AppContext.Provider

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { doctors } from '../assets/assets'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+import { doctors} from '../assets/assets'
+
 
 const TopDoctors = () => {
 const navigate=useNavigate();
+const { doctors } = useContext(AppContext);
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
@@ -11,7 +14,7 @@ const navigate=useNavigate();
             <p className='sm:w-1/3 text-center  text-sm '>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid [grid-template-columns:repeat(auto-fill,minmax(150px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                 {doctors.slice(0,10).map((item,index)=>(
-                    <div onClick={()=>navigate(`/appointments/${item.id}`)} className='border border-blue-300 rounded-b-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <div onClick={()=>navigate(`/appointments/${item._id}`)} className='border border-blue-300 rounded-b-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                        <img className='bg-blue-50 ' src={item.image} alt="" />
                        <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
